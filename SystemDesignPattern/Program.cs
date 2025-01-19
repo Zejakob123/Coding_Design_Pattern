2ccd202e7f68f9527a86ac6dfced6e083608e9a2
Zejakob123 <jzleong@outlook.com>
2025-01-19 15:34:09 +0800

    Init
    - Setup the project folder for system design pattern

---
+++ SystemDesignPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using src.factory;
+
+namespace SystemDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var a = new Class1();
+            a.Method1();
+        }
+    }
+}
---
+++ SystemDesignPattern/src/creational/factory/a1.cs
@@ -0,0 +1,11 @@
+// File1.cs
+namespace src.factory
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1 in Class1");
+        }
+    }
+}
